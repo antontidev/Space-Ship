4d6a43df0531ea0d25f1c59097cf84994e1a810c
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-18 18:18:00 +0300

    Obj spawn

---
+++ Assets/Scripts/PartsSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsSpawner : MonoBehaviour
+{
+    [SerializeField]
+        private GameObject shipPrefab;
+    [SerializeField]
+
+    void Start()
+    {
+        
+        for (int i = 1; i < 10; i++)
+        {
+        Instantiate(shipPrefab, Random.onUnitSphere * 10, transform.rotation);
+        }       
+    }
+}
