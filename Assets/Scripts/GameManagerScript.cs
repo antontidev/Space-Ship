00bb90baa805de1e149d2cce972d91cf60f26f77
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-18 19:59:04 +0300

    Game manager

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    GameObject gameTimer = GameObject.Find("Timer");
+    Timer theTimer = gameTimer.GetComponent<Timer>();
+
+    void Update()
+    {
+        if (theTimer.timer <= 0)
+        {
+            theTimer.ResetTimer();
+            goToLevelOne();
+        }
+    }
+
+    void changeLevel()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void goToLevelOne()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
