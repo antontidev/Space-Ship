9bb44f2de0bf02676ac42955ff23b4405292989b
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-18 15:15:09 +0300

    Planetary gravity implementation

---
+++ Assets/Scripts/phys/PhysObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+
+public class PhysObj : MonoBehaviour
+{
+
+    Gravity planet;
+
+    void Awake()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<Gravity>();
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        planet.Attract(transform);
+    }
+}
