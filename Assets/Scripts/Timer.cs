81372993978f4d32f544790ec45bab303f875068
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-18 18:28:59 +0300

    New spawner

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+
+    void Start()
+    {
+        timer = 60;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+
+    void ResetTimer()
+    {
+        timer = 60;
+    }
+}
