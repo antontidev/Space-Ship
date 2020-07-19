f8f1725d9b500e0d8d5c53690c3d5b72d9f3bced
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-19 16:08:30 +0300

    es11

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,6 +7,9 @@ public class Rocket : MonoBehaviour
     [SerializeField]
     public List<GameObject> trueParts;
 
+    [SerializeField]
+    private List<Transform> positions;
+
     private Dictionary<GameObject, bool> ready;
 
     private void Start()
@@ -33,5 +36,35 @@ public class Rocket : MonoBehaviour
             }
         }
         return false;
+    }
+
+    public void Handle(GameObject part)
+    {
+        if (CheckPart(part) == true)
+        {
+            part.transform.parent = gameObject.transform;
+
+            switch (part.tag)
+            {
+                case "Bottom":
+                    part.transform.position = GameObject.FindGameObjectWithTag("ModuleOne").transform.position;
+                    part.transform.rotation = GameObject.FindGameObjectWithTag("ModuleOne").transform.rotation;
+                    break;
+                case "Middle":
+                    part.transform.position = GameObject.FindGameObjectWithTag("ModuleTwo").transform.position;
+                    part.transform.rotation = GameObject.FindGameObjectWithTag("ModuleTwo").transform.rotation;
+                    break;
+                case "Top":
+                    part.transform.position = GameObject.FindGameObjectWithTag("ModuleThree").transform.position;
+                    part.transform.rotation = GameObject.FindGameObjectWithTag("ModuleThree").transform.rotation;
+                    break;
+            }
+
+            part.GetComponent<Rigidbody>().useGravity = false;
+            part.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+            part.GetComponent<PhysObj>().enabled = false;
+
+        }
     }
 }
