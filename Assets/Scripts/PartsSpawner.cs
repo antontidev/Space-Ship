65a4075de365aa3a870610eb0c0f27c8894c1853
Matt Burlakov <64248507+mattburlakov@users.noreply.github.com>
2020-07-19 14:57:25 +0300

    es11

--- Assets/Scripts/PartsSpawner.cs
+++ Assets/Scripts/PartsSpawner.cs
@@ -16,13 +16,13 @@ public class PartsSpawner : MonoBehaviour
     {
         foreach (var el in parts)
         {
-            var part = Instantiate(el, Random.onUnitSphere * 1, transform.rotation);
+            var part = Instantiate(el, Random.onUnitSphere * 5, transform.rotation);
             //part.GetComponent<ShipPart>().onClick += PartClicked;
             yield return null;
         }
         foreach (var el in trueModules)
         {
-            var part = Instantiate(el, Random.onUnitSphere * 1, transform.rotation);
+            var part = Instantiate(el, Random.onUnitSphere * 5, transform.rotation);
             //part.GetComponent<ShipPart>().onClick += PartClicked;
             yield return null;
         }
