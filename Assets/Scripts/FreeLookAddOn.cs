27691424f1a719b3ad4a8e2627e5adcebee60842
antontidev <sanjaplushka@gmail.com>
2020-07-29 20:56:57 +0500

    Remove unused camera callbacks

--- Assets/Scripts/FreeLookAddOn.cs
+++ Assets/Scripts/FreeLookAddOn.cs
@@ -57,36 +57,7 @@ public class FreeLookAddOn : MonoBehaviour
     {
         var delta = input.EndPosition - input.PreviousPosition;
 
-        Debug.Log(delta);
-
         _lastX = delta.x;
         _lastY = delta.y;
-    }
-
-    private void Touch_onFingerMove(SwipeInput input)
-    {
-        var delta = input.EndPosition - input.PreviousPosition;
-
-        Debug.Log(delta);
-        //_freeLookComponent.m_XAxis.Value += dir.x * Time.deltaTime * LookSpeed;
-        //_freeLookComponent.m_YAxis.Value += dir.y * Time.deltaTime * LookSpeed;
-    }
-
-    public void OnLookSwipe(SwipeInput swipeInput)
-    {
-        float duration = Convert.ToSingle(swipeInput.SwipeDuration);
-
-        Vector2 dir = swipeInput.SwipeDirection;
-
-        var dir2 = new Vector3(swipeInput.SwipeDirection.x, swipeInput.SwipeDirection.y, 0);
-        var camRotation = Quaternion.Euler(dir2);
-
-        dir.y = InvertY ? -dir.y : dir.y;
-
-        dir.x *= 180f;
-
-        _freeLookComponent.m_XAxis.Value += dir.x * duration * Time.deltaTime * LookSpeed;
-        _freeLookComponent.m_YAxis.Value += dir.y * duration * Time.deltaTime * LookSpeed;
-
     }
 }
